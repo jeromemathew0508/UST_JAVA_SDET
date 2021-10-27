4118f8000011f4176bc60efa77f91cf1931bb55a
jeromemathew0508 <92138311+jeromemathew0508@users.noreply.github.com>
2021-10-27 11:22:23 +0530

    Add files via upload

---
+++ Testing/Selenium/PG9.java
@@ -0,0 +1,25 @@
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+public class PG9 {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\ustjavasdetb415\\Downloads\\chromedriver_win32\\chromedriver.exe");
+        String baseUrl = "http://demo.guru99.com/test/upload/";
+        WebDriver driver = new ChromeDriver();
+
+        driver.get(baseUrl);
+        WebElement uploadElement = driver.findElement(By.id("uploadfile_0"));
+
+        // enter the file path onto the file-selection input field
+        uploadElement.sendKeys("C:\\newhtml.html");
+
+        // check the "I accept the terms of service" check box
+        driver.findElement(By.id("terms")).click();
+
+        // click the "UploadFile" button
+        driver.findElement(By.name("send")).click();
+        }
+}
+
+
---
+++ Testing/Selenium/Sample1.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Sample1 {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\ustjavasdetb415\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		WebDriver d = new ChromeDriver();
+		d.navigate().to("http://www.google.com/");
+		String s = d.getTitle();
+		System.out.println(s);
+		s.equals("Google");
+	}
+
+}
